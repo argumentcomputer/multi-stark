dc8d5a0acd4560dd0c09e63f6cd9a4b359045d26
Artem Storozhuk <storojs72@gmail.com>
2025-08-14 00:22:54 +0300

    feat: Implement U32Or chip

--- src/chips/utility.rs
+++ src/chips/utility.rs
@@ -22,6 +22,9 @@ mod tests {
     // Main trace consists of multiplicity for 'u8_or' and 'range_check' operations
     const U8_OR_PAIR_RANGE_CHECK_TRACE_WIDTH: usize = 2;
 
+    // multiplicity, left0, left1, left2, left3, right0, right1, right2, right3, or0, or1, or2, or3
+    const U32_OR_TRACE_WIDTH: usize = 13;
+
     enum UtilityChip {
         U32FromLeBytes,
         U32Or,
@@ -32,10 +35,10 @@ mod tests {
     impl UtilityChip {
         fn position(&self) -> usize {
             match self {
-                Self::U32FromLeBytes => 0,
-                Self::U32Or => 1,
-                Self::U8Or => 2,
-                Self::U8PairRangeCheck => 3,
+                Self::U8Or => 0,
+                Self::U8PairRangeCheck => 1,
+                Self::U32FromLeBytes => 2,
+                Self::U32Or => 3,
             }
         }
     }
@@ -44,7 +47,7 @@ mod tests {
         fn width(&self) -> usize {
             match self {
                 Self::U32FromLeBytes => U32_FROM_LE_BYTES_TRACE_WIDTH,
-                Self::U32Or => todo!(),
+                Self::U32Or => U32_OR_TRACE_WIDTH,
                 Self::U8Or | Self::U8PairRangeCheck => U8_OR_PAIR_RANGE_CHECK_TRACE_WIDTH,
             }
         }
@@ -66,8 +69,7 @@ mod tests {
                     }
                     Some(RowMajorMatrix::new(trace_values, PREPROCESSED_TRACE_WIDTH))
                 }
-                Self::U32FromLeBytes => None,
-                Self::U32Or => None,
+                Self::U32FromLeBytes | Self::U32Or => None,
             }
         }
     }
@@ -79,20 +81,17 @@ mod tests {
     {
         fn eval(&self, _builder: &mut AB) {
             match self {
-                Self::U32FromLeBytes => {}
-                Self::U32Or => {}
-                Self::U8Or => {}
-                Self::U8PairRangeCheck => {}
+                Self::U32FromLeBytes | Self::U32Or | Self::U8Or | Self::U8PairRangeCheck => {}
             }
         }
     }
 
     impl UtilityChip {
         fn lookups(&self) -> Vec<Lookup<SymbExpr>> {
-            let u32_from_le_bytes_idx = Self::U32FromLeBytes.position();
-            // let u32_or_idx = Self::U32Or.position();
             let u8_or_idx = Self::U8Or.position();
             let u8_pair_range_check_idx = Self::U8PairRangeCheck.position();
+            let u32_from_le_bytes_idx = Self::U32FromLeBytes.position();
+            let u32_or_idx = Self::U32Or.position();
 
             match self {
                 Self::U32FromLeBytes => {
@@ -130,9 +129,37 @@ mod tests {
                     ]
                 }
                 Self::U32Or => {
-                    todo!();
-                }
+                    let mut lookups = vec![Lookup::pull(
+                        var(0),
+                        vec![
+                            SymbExpr::from_usize(u32_or_idx),
+                            var(1)
+                                + var(2) * SymbExpr::from_u32(256)
+                                + var(3) * SymbExpr::from_u32(256 * 256)
+                                + var(4) * SymbExpr::from_u32(256 * 256 * 256),
+                            var(5)
+                                + var(6) * SymbExpr::from_u32(256)
+                                + var(7) * SymbExpr::from_u32(256 * 256)
+                                + var(8) * SymbExpr::from_u32(256 * 256 * 256),
+                            var(9)
+                                + var(10) * SymbExpr::from_u32(256)
+                                + var(11) * SymbExpr::from_u32(256 * 256)
+                                + var(12) * SymbExpr::from_u32(256 * 256 * 256),
+                        ],
+                    )];
 
+                    lookups.extend((0..6).map(|i| {
+                        Lookup::push(
+                            SymbExpr::ONE,
+                            vec![
+                                SymbExpr::from_usize(u8_pair_range_check_idx),
+                                var(i + 1),
+                                var(i + 7),
+                            ],
+                        )
+                    }));
+                    lookups
+                }
                 Self::U8Or | Self::U8PairRangeCheck => {
                     vec![
                         Lookup::pull(
@@ -170,12 +197,24 @@ mod tests {
             let mut byte_or_values_from_claims = vec![];
             let mut byte_range_check_values_from_claims = vec![];
             let mut u32_from_le_bytes_values_from_claims = vec![];
+            let mut u32_or_values_from_claims = vec![];
 
             for claim in self.claims.clone() {
                 // we should have at least chip index
                 assert!(!claim.is_empty(), "wrong claim format");
                 match claim[0].as_canonical_u64() {
                     0u64 => {
+                        // This is our U8Or claim. We should have chip_idx, A, B, A or B (where A, B are bytes)
+                        assert_eq!(claim.len(), 4, "[U8Or] wrong claim format");
+                        byte_or_values_from_claims.push((claim[1], claim[2], claim[3]));
+                    }
+                    1u64 => {
+                        /* This is our U8PairRangeCheck claim. We should have chip_idx, A, B */
+
+                        assert_eq!(claim.len(), 3, "[U8PairRangeCheck] wrong claim format");
+                        byte_range_check_values_from_claims.push((claim[1], claim[2]));
+                    }
+                    2u64 => {
                         // this is out u32_from_le_bytes chip. We should have chip_idx, byte0, byte1, byte2, byte3, u32
                         assert_eq!(claim.len(), 6);
 
@@ -189,22 +228,15 @@ mod tests {
                         u32_from_le_bytes_values_from_claims
                             .push((byte0_val, byte1_val, byte2_val, byte3_val, u32_val));
                     }
-                    1u64 => {
-                        // u32 or
-                        todo!();
-                    }
-
-                    2u64 => {
-                        // This is our U8Or claim. We should have chip_idx, A, B, A or B (where A, B are bytes)
-                        assert_eq!(claim.len(), 4, "[U8Or] wrong claim format");
-                        byte_or_values_from_claims.push((claim[1], claim[2], claim[3]));
-                    }
-
                     3u64 => {
-                        /* This is our U8PairRangeCheck claim. We should have chip_idx, A, B */
+                        // this is out u32_or chip. We should have: chip_idx, left, right, or
+                        assert_eq!(claim.len(), 4);
 
-                        assert_eq!(claim.len(), 3, "[U8PairRangeCheck] wrong claim format");
-                        byte_range_check_values_from_claims.push((claim[1], claim[2]));
+                        let left = u32::try_from(claim[1].as_canonical_u64()).unwrap();
+                        let right = u32::try_from(claim[2].as_canonical_u64()).unwrap();
+                        let or = u32::try_from(claim[3].as_canonical_u64()).unwrap();
+
+                        u32_or_values_from_claims.push((left, right, or));
                     }
 
                     _ => {
@@ -213,6 +245,62 @@ mod tests {
                 }
             }
 
+            // u32_or
+            let mut u32_or_trace_values =
+                Vec::<Val>::with_capacity(u32_or_values_from_claims.len());
+            if u32_or_values_from_claims.is_empty() {
+                u32_or_trace_values = Val::zero_vec(U32_OR_TRACE_WIDTH);
+
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+            } else {
+                for (left, right, or) in u32_or_values_from_claims {
+                    let computed = left | right;
+                    debug_assert_eq!(or, computed);
+
+                    let left_bytes: [u8; 4] = left.to_le_bytes();
+                    let right_bytes: [u8; 4] = right.to_le_bytes();
+                    let or_bytes: [u8; 4] = or.to_le_bytes();
+
+                    u32_or_trace_values.push(Val::ONE); // multiplicity
+                    u32_or_trace_values.extend_from_slice(left_bytes.map(Val::from_u8).as_slice());
+                    u32_or_trace_values.extend_from_slice(right_bytes.map(Val::from_u8).as_slice());
+                    u32_or_trace_values.extend_from_slice(or_bytes.map(Val::from_u8).as_slice());
+
+                    /* we send decomposed bytes to U8PairRangeCheck chip, relying on lookup constraining */
+
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(left_bytes[0]), Val::from_u8(right_bytes[2])));
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(left_bytes[1]), Val::from_u8(right_bytes[3])));
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(left_bytes[2]), Val::from_u8(or_bytes[0])));
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(left_bytes[3]), Val::from_u8(or_bytes[1])));
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(right_bytes[0]), Val::from_u8(or_bytes[2])));
+                    byte_range_check_values_from_claims
+                        .push((Val::from_u8(right_bytes[1]), Val::from_u8(or_bytes[3])));
+                }
+            }
+            let mut u32_or_trace = RowMajorMatrix::new(u32_or_trace_values, U32_OR_TRACE_WIDTH);
+            let height = u32_or_trace.height().next_power_of_two();
+            let zero_rows = height - u32_or_trace.height();
+            for _ in 0..zero_rows {
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+                byte_range_check_values_from_claims.push((Val::ZERO, Val::ZERO));
+            }
+            u32_or_trace.pad_to_height(height, Val::ZERO);
+
+            // u32_from_le
             let mut u32_from_le_bytes_trace_values =
                 Vec::<Val>::with_capacity(u32_from_le_bytes_values_from_claims.len());
             if u32_from_le_bytes_values_from_claims.is_empty() {
@@ -286,7 +374,9 @@ mod tests {
                     u8_or_range_check_trace_values,
                     U8_OR_PAIR_RANGE_CHECK_TRACE_WIDTH,
                 ),
+                // range check trace is entirely preprocessed, so it is "free"
                 u32_from_le_bytes_trace,
+                u32_or_trace,
             ];
 
             (traces.clone(), SystemWitness::from_stage_1(traces, system))
@@ -307,10 +397,11 @@ mod tests {
             UtilityChip::U32FromLeBytes,
             UtilityChip::U32FromLeBytes.lookups(),
         );
+        let u32_or_circuit = LookupAir::new(UtilityChip::U32Or, UtilityChip::U32Or.lookups());
 
         let (system, prover_key) = System::new(
             commitment_parameters,
-            vec![u8_circuit, u32_from_le_bytes_circuit],
+            vec![u8_circuit, u32_from_le_bytes_circuit, u32_or_circuit], // order matters
         );
 
         let claims = UtilityChipClaims {
@@ -359,10 +450,11 @@ mod tests {
             UtilityChip::U32FromLeBytes,
             UtilityChip::U32FromLeBytes.lookups(),
         );
+        let u32_or_circuit = LookupAir::new(UtilityChip::U32Or, UtilityChip::U32Or.lookups());
 
         let (system, prover_key) = System::new(
             commitment_parameters,
-            vec![u8_circuit, u32_from_le_bytes_circuit],
+            vec![u8_circuit, u32_from_le_bytes_circuit, u32_or_circuit], // order matters!
         );
 
         let claims = UtilityChipClaims {
@@ -370,6 +462,53 @@ mod tests {
                 [
                     vec![Val::from_usize(UtilityChip::U8Or.position())],
                     vec![Val::from_u8(byte0), Val::from_u8(byte1), Val::from_u8(or)],
+                ]
+                .concat(),
+            ],
+        };
+
+        let (_traces, witness) = claims.witness(&system);
+
+        let claims_slice: Vec<&[Val]> = claims.claims.iter().map(|v| v.as_slice()).collect();
+        let claims_slice: &[&[Val]] = &claims_slice;
+
+        let fri_parameters = FriParameters {
+            log_final_poly_len: 0,
+            num_queries: 64,
+            proof_of_work_bits: 0,
+        };
+
+        let proof =
+            system.prove_multiple_claims(fri_parameters, &prover_key, claims_slice, witness);
+        system
+            .verify_multiple_claims(fri_parameters, claims_slice, &proof)
+            .expect("verification issue");
+    }
+
+    #[test]
+    fn test_u32_or() {
+        let left = 0xabcd1234u32;
+        let right = 0x998123ffu32;
+        let or = left | right;
+
+        let commitment_parameters = CommitmentParameters { log_blowup: 1 };
+        let u8_circuit = LookupAir::new(UtilityChip::U8Or, UtilityChip::U8Or.lookups());
+        let u32_or_circuit = LookupAir::new(UtilityChip::U32Or, UtilityChip::U32Or.lookups());
+        let u32_from_le_bytes_circuit = LookupAir::new(
+            UtilityChip::U32FromLeBytes,
+            UtilityChip::U32FromLeBytes.lookups(),
+        );
+
+        let (system, prover_key) = System::new(
+            commitment_parameters,
+            vec![u8_circuit, u32_from_le_bytes_circuit, u32_or_circuit], // order matters
+        );
+
+        let claims = UtilityChipClaims {
+            claims: vec![
+                [
+                    vec![Val::from_usize(UtilityChip::U32Or.position())],
+                    vec![Val::from_u32(left), Val::from_u32(right), Val::from_u32(or)],
                 ]
                 .concat(),
             ],
